8cf1ae475b9bc538b8fbb3b512f465e261d026ed
konaOnTheHub <nagy.petike2@gmail.com>
2025-03-19 18:16:59 +0000

    first commit

---
+++ backend/Models/User.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace backend.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+        public string Role { get; set; } = "claimant"; // 'admin', 'staff', 'claimant'
+    }
+}
